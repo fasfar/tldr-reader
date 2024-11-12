aa1723746646838069895411d3a56822b4406c50
fasfar <44534592+fasfar@users.noreply.github.com>
2024-11-11 21:42:33 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	//"strings"
+	"github.com/fasfar/tldr-reader/mailman"
+	//"github.com/fasfar/tldr-reader/postgres"
+)
+
+func main() {
+	mailman.GetMessages()
+	//postgres.GetDBConnection()
+}
